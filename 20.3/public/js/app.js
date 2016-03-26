d6205cb03e095f7f023ca311f593605352f9defb
C-Das <chinmay98@gmail.com>
2016-03-26 11:57:38 -0400

    Added app.js and index.html files

---
+++ 20.3/public/js/app.js
@@ -0,0 +1,13 @@
+angular.module('OMDBApp',['ui.router'])
+.config(function($stateProvider, $urlRouterProvider, $locationProvider){
+
+  $urlRouterProvider.otherwise('/home');
+
+  $stateProvider
+  .state ('home',{
+    url: '/home',
+    templateUrl:'/views/home.html';
+  })
+
+  $locationProvider.html5Mode(true);
+});
---
+++ 20.3/server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var logger = require('morgan');
+
+var app = express();
+var PORT = 3000;
+
+app.use(logger('dev'));
+app.use(express.static('public'));
+
+app.get('*',function(req,res){
+  res.sendFile(process.cwd()+'/public/index.html');
+});
+
+app.listen(PORT,function(){
+  console.log("Application is listening on PORT"+PORT);
+})
